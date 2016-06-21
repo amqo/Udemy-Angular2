ed9820d9b7bdf33e0a139df01ae5c7858576273b
Alberto Muñoz <alberto.mqo@gmail.com>
2016-06-21 12:24:17 +0200

    Define the form inside constructor in a more compact way.

--- Introduction Project/app/directives/components/signup-form.component.js
+++ Introduction Project/app/directives/components/signup-form.component.js
@@ -22,11 +22,10 @@ System.register(['angular2/core', 'angular2/common'], function(exports_1, contex
             }],
         execute: function() {
             SignupFormComponent = (function () {
-                function SignupFormComponent() {
-                    //Model driven form
-                    this.form = new common_1.ControlGroup({
-                        username: new common_1.Control('', common_1.Validators.required),
-                        password: new common_1.Control('', common_1.Validators.required)
+                function SignupFormComponent(formBuilder) {
+                    this.form = formBuilder.group({
+                        username: ['', common_1.Validators.required],
+                        password: ['', common_1.Validators.required]
                     });
                 }
                 SignupFormComponent.prototype.signup = function () {
@@ -37,7 +36,7 @@ System.register(['angular2/core', 'angular2/common'], function(exports_1, contex
                         selector: 'signup-form',
                         templateUrl: 'app/templates/signup-form.template.html'
                     }), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [common_1.FormBuilder])
                 ], SignupFormComponent);
                 return SignupFormComponent;
             }());
